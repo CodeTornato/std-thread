05bf5e9bec426ee5332ab1b37f19381477a503cf
CodeTornato <118702310+CodeTornato@users.noreply.github.com>
2023-08-07 09:36:28 +0800

    Create hello_thread.cpp

---
+++ hello_thread.cpp
@@ -0,0 +1,12 @@
+#include <pthread>
+#include <isotream>
+
+void helloThread(int num){
+  std::cout << "Thead " << num << "is being called! " << std::endl; 
+}
+
+int main(){
+  std::thread t1(helloThread,2);
+  t1.join();
+  return 0;
+}
