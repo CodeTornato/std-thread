d2ad9ad134ba430da0e7fe7c0cc12af79d2b631b
CodeTornato <118702310+CodeTornato@users.noreply.github.com>
2023-08-08 09:51:52 +0800

    Update share_data.cpp

--- share_data.cpp
+++ share_data.cpp
@@ -4,12 +4,12 @@
 
 std::mutex num_mutex;
 
-void printNum(double num){
+void printNum(double& num){
   std::lock_guard<std::mutex> num_lock(num_mutex);
   std::cout << "number is: " << num << std::endl;
 }
 
-void addNum(double num){
+void addNum(double& num){
   std::lock_guard<std::mutex> num_lock(num_mutex);
   // modifying shared number here by adding 1
   num+=1;
@@ -18,9 +18,14 @@ void addNum(double num){
 int main(){
 
   double num = 0;
-  std::thread t1(printNum,num);
-  std::thread t2(addNum,num);
-  t1.join();
-  t2.join();
-  // I want to execute multipy time here , how can I do it ?
+  // I want to execute multipy time here , here is how I do it..
+  int time_to_execute = 5;
+  for(int i = 0; i < time_to_execute; i++){
+      std::thread t1(printNum,num);
+      std::thread t2(addNum,num);
+      t1.join();
+      t2.join();
+  }
+
+  return 0;
 }
