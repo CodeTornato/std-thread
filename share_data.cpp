9e0c58fc7c935ca805da035d91b23d0182172844
CodeTornato <118702310+CodeTornato@users.noreply.github.com>
2023-08-08 09:55:04 +0800

    Update share_data.cpp

--- share_data.cpp
+++ share_data.cpp
@@ -21,8 +21,8 @@ int main(){
   // I want to execute multipy time here , here is how I do it..
   int time_to_execute = 5;
   for(int i = 0; i < time_to_execute; i++){
-      std::thread t1(printNum,num);
-      std::thread t2(addNum,num);
+      std::thread t1(printNum,std::ref(num));
+      std::thread t2(addNum,std::ref(num));
       t1.join();
       t2.join();
   }
