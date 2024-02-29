63e70852b2ae4ebe474e54fd4ffb87cfef37e41b
CodeTornato <118702310+CodeTornato@users.noreply.github.com>
2024-02-28 18:47:43 -1100

    Update dead_lock.cpp

--- dead_lock.cpp
+++ dead_lock.cpp
@@ -1,2 +1,5 @@
 # in the book, I learn one of the side effect of using std::mutex to luck the function, is that it might introduce deadlock problem
 # what is deadlock, guess locking the thread it can not exit, and other thread will not able to access share data
+#include <isotream>
+#include <mutex>
+#include <thread>
