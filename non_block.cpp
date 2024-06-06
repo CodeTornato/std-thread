4edcf176c5aa74da9e8003fac9ef319c02625336
CodeTornato <118702310+CodeTornato@users.noreply.github.com>
2024-06-06 14:16:27 +0800

    Create non_block.cpp

---
+++ non_block.cpp
@@ -0,0 +1,15 @@
+#include <thread>
+#include <iostream>
+
+
+// implement non lock thread communication and synchronization?
+
+// a buffer area and provider - consumer pattern
+
+char data_pool[256];
+
+
+int main(){
+
+  
+}
